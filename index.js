b570d182172a8f162b2daede9bbdce8ef76b7495
Emarilyne Wakio <emarilynew@gmail.com>
2024-04-14 14:27:05 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, apple) {
+  return drivers.filter
+  (driver => driver.toLowerCase() === apple.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, s) {
+    return drivers.filter
+    (driver => driver.startsWith(s));
+}
+function matchName(drivers, apple) {
+    return drivers.filter
+    (driver => driver.name === apple);
+}
